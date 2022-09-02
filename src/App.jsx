a3f60cc636f3895b52fb9aa10a0da90b580ed776
Wesley Nilton <wesleynlimac@gmail.com>
2022-09-02 12:21:42 -0300

    Create application structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-function App() {
-  return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Home from './pages/Home';
+import Company from './pages/Company';
+import NewProject from './pages/NewProject';
+import Contact from './pages/Contact';
+
+function App(){
+  return(
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/newproject'>Novo Projeto</Link>
+        <Link to='/contact'>Contato</Link>
+      </div>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/company' element={<Company />} />
+        <Route path='/newproject' element={<NewProject />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <p>Footer</p>
+    </Router>
   )
 }
 
