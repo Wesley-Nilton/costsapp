1e7c55cf7291c555bd772e196e67f2ac48fb6738
Wesley Nilton <wesleynlimac@gmail.com>
2022-09-09 07:52:58 -0300

    Create select component

--- src/components/ProjectForm/ProjectForm.jsx
+++ src/components/ProjectForm/ProjectForm.jsx
@@ -1,4 +1,5 @@
 import Input from '../Input/Input';
+import Select from '../Select/Select';
 import './ProjectForm.css';
 
 function ProjectForm(){
@@ -6,11 +7,7 @@ function ProjectForm(){
         <form className='form'>
             <Input type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome do projeto' />
             <Input type='number' text='Orçamento do Projeto' name='budget' placeholder='Insira o orçamento total' />
-            <div>
-                <select name="category-id">
-                    <option disabled selected>Selecione a categoria</option>
-                </select>
-            </div>
+            <Select name='category-id' text='Selecione a categoria' />
             <input type="submit" value="Criar Projeto" />
         </form>
     )
