37fd96e2ba858ec116ec9ee4021630aecceab781
Wesley Nilton <wesleynlimac@gmail.com>
2022-09-10 07:54:07 -0300

    Create submitbutton component

--- src/components/ProjectForm/ProjectForm.jsx
+++ src/components/ProjectForm/ProjectForm.jsx
@@ -1,14 +1,15 @@
 import Input from '../Input/Input';
 import Select from '../Select/Select';
+import SubmitButton from '../SubmitButton/SubmitButton';
 import './ProjectForm.css';
 
-function ProjectForm(){
+function ProjectForm({btnText}){
     return(
         <form className='form'>
             <Input type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome do projeto' />
             <Input type='number' text='Orçamento do Projeto' name='budget' placeholder='Insira o orçamento total' />
             <Select name='category-id' text='Selecione a categoria' />
-            <input type="submit" value="Criar Projeto" />
+            <SubmitButton text={btnText} />
         </form>
     )
 }
