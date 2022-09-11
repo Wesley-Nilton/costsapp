6a942ebd814a2691a597c7b4f33eec46c24a24d2
Wesley Nilton <wesleynlimac@gmail.com>
2022-09-11 06:56:39 -0300

    Add request to the database

--- src/components/ProjectForm/ProjectForm.jsx
+++ src/components/ProjectForm/ProjectForm.jsx
@@ -1,14 +1,29 @@
+import {useEffect, useState} from 'react';
 import Input from '../Input/Input';
 import Select from '../Select/Select';
 import SubmitButton from '../SubmitButton/SubmitButton';
 import './ProjectForm.css';
 
 function ProjectForm({btnText}){
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers:{
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response => response.json())
+        .then(data => setCategories(data))
+        .catch((err => console.log(err)))
+    }, []);
+
     return(
         <form className='form'>
             <Input type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome do projeto' />
             <Input type='number' text='Orçamento do Projeto' name='budget' placeholder='Insira o orçamento total' />
-            <Select name='category-id' text='Selecione a categoria' />
+            <Select name='category-id' text='Selecione a categoria' options={categories} />
             <SubmitButton text={btnText} />
         </form>
     )
