20cacea5001b4a2d0e9625ab07b0ba0884e68ffc
Wesley Nilton <wesleynlimac@gmail.com>
2022-09-14 07:33:35 -0300

    Insert information into the database

--- src/components/ProjectForm/ProjectForm.jsx
+++ src/components/ProjectForm/ProjectForm.jsx
@@ -4,8 +4,9 @@ import Select from '../Select/Select';
 import SubmitButton from '../SubmitButton/SubmitButton';
 import './ProjectForm.css';
 
-function ProjectForm({btnText}){
+function ProjectForm({handleSubmit, btnText, projectData}){
     const [categories, setCategories] = useState([]);
+    const [project, setProject] = useState(projectData || {});
 
     useEffect(() => {
         fetch('https://my-json-server.typicode.com/Wesley-Nilton/costsapp/categories', {
@@ -19,11 +20,28 @@ function ProjectForm({btnText}){
         .catch((err => console.log(err)))
     }, []);
 
+    const submit = e => {
+        e.preventDefault();
+        handleSubmit(project);
+    }
+
+    function handleChange(e){
+        setProject({...project, [e.target.name]: e.target.value});
+    }
+
+    function handleCategory(e){
+        setProject({...project, category: {
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text,
+            }
+        });
+    }
+
     return(
-        <form className='form'>
-            <Input type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome do projeto' />
-            <Input type='number' text='Orçamento do Projeto' name='budget' placeholder='Insira o orçamento total' />
-            <Select name='category-id' text='Selecione a categoria' options={categories} />
+        <form onSubmit={submit} className='form'>
+            <Input handleOnChange={handleChange} type='text' text='Nome do Projeto' name='name' placeholder='Insira o nome do projeto' value={project.name ? project.name : ''} />
+            <Input handleOnChange={handleChange} type='number' text='Orçamento do Projeto' name='budget' placeholder='Insira o orçamento total' value={project.budget ? project.budget : ''} />
+            <Select handleOnChange={handleCategory} name='category-id' text='Selecione a categoria' options={categories} value={project.category ? project.category.id : ''} />
             <SubmitButton text={btnText} />
         </form>
     )
--- src/pages/NewProject/NewProject.jsx
+++ src/pages/NewProject/NewProject.jsx
@@ -1,12 +1,32 @@
+import {useNavigate} from 'react-router-dom';
 import ProjectForm from '../../components/ProjectForm/ProjectForm';
 import './NewProject.css';
 
 function NewProject(){
+    const navigate = useNavigate();
+
+    function createPost(project){
+        // initialize cost and services
+        project.cost = 0;
+        project.services = [];
+
+        fetch('https://my-json-server.typicode.com/Wesley-Nilton/costsapp/projects', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+        .then(response => response.json())
+        .then(data => navigate('/projects', {message: 'Projeto criado com sucesso!'}))
+        .catch(err => console.log(err))
+    }
+
     return(
         <div className='newproject-container'>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText='Criar Projeto' />
+            <ProjectForm handleSubmit={createPost} btnText='Criar Projeto' />
         </div>
     )
 }
