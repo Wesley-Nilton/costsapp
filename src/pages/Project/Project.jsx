c936ac1888e0d744707159430de4e6395a85ec6a
Wesley Nilton <wesleynlimac@gmail.com>
2022-10-01 07:32:08 -0300

    Add project edit function

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -2,19 +2,23 @@ import {useParams} from 'react-router-dom';
 import {useState, useEffect} from 'react';
 import Loading from '../../components/Loading/Loading';
 import Container from '../../components/Container/Container';
+import ProjectForm from '../../components/ProjectForm/ProjectForm';
+import Message from '../../components/Message/Message';
 import './Project.css';
 
 function Project(){
     const {id} = useParams();
     const [project, setProject] = useState([]);
     const [showProjectForm, setShowProjectForm] = useState(false);
+    const [message, setMessage] = useState();
+    const [type, setType] = useState();
 
     useEffect(() =>{
         setTimeout(() => {
             fetch(`https://my-json-server.typicode.com/Wesley-Nilton/costsapp/projects/${id}`, {
             method: 'GET',
             headers: {
-                'Content-type': 'application/json',
+                'Content-Type': 'application/json',
             },
             })
             .then(response => response.json())
@@ -22,6 +26,30 @@ function Project(){
             .catch(err => console.log(err))
         }, 300)
     }, [id]);
+
+    function editPost(project){
+        if(project.budget < project.cost){
+            setMessage('O orçamento não pode ser menor que o custo do projeto!');
+            setType('error');
+            return false;
+        }
+
+        fetch(`https://my-json-server.typicode.com/Wesley-Nilton/costsapp/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project),
+            })
+            .then(response => response.json())
+            .then(data => {
+                setProject(data);
+                setShowProjectForm(false);
+                setMessage('Projeto atualizado!');
+                setType('success');
+            })
+            .catch(err => console.log(err))
+    }
 
     function toggleProjectForm(){
         setShowProjectForm(!showProjectForm);
@@ -32,6 +60,7 @@ function Project(){
             {project.name ? (
                 <div className='project-details'>
                     <Container customClass='column'>
+                        {message && <Message type={type} msg={message} />}
                         <div className='details-container'>
                             <h1>Projeto: {project.name}</h1>
                             <button className='btn' onClick={toggleProjectForm}>
@@ -45,7 +74,7 @@ function Project(){
                                 </div>
                             ) : (
                                 <div className='project-info'>
-                                    <p>detalhes do projeto</p>
+                                    <ProjectForm handleSubmit={editPost} btnText='Concluir Edição' projectData={project} />
                                 </div>
                             )}
                         </div>
